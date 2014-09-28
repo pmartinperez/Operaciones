8a9cf6b549607b1c0ff23a33ea646120c40573bf
Patricia <Patricia@Patri>
2014-09-29 01:57:01 +0200

    New commit

---
+++ src/Operacion.java
@@ -0,0 +1,22 @@
+
+/**
+ *
+ * @author Patricia
+ */
+public class Operacion {
+    public static void main (String[] args){
+        //Variables
+        int a= 10;
+        int b= 2;
+        
+        //Operaciones
+        System.out.println ("La suma de los numeros es: " + (a+b));
+        System.out.println ("La resta de los numeros es: " + (a-b));
+        System.out.println ("El producto de los numeros es: " + (a*b));
+        System.out.println ("La division de los numeros es: " + (a/b));
+        System.out.println ("El resto de la división de los numeros es: " + (a%b));
+        
+            
+    }
+    
+}
